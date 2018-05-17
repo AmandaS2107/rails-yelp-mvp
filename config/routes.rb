6db29b48f13dbd5590bebc691efff11b93409db2
AmandaS <mandyschmitz2107@gmail.com>
2018-05-17 10:27:07 +0200

    almost done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
-Rails.application.routes.draw do
+Rails.application
+  #  get 'restaurants/index'
+  #  get 'restaurants/show'
+  #  get 'restaurants/new'
+  #  post
+  #  get 'reviews/index'
+  #  get 'reviews/new'
+   # post
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+  resources :reviews, only: [ :new, :create ]
+  end
 end
